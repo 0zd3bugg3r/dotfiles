7aedb86d7258f807bfa10b546861d1efbca1368d
Ayşe <43453523+0zd3bugg3r@users.noreply.github.com>
2018-09-21 14:24:23 +0300

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,4 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias debug='cd /home/ayshe/Desktop/debugTools'
 alias dirty='cd /home/ayshe/Documents/leaks'
-alias lastdump=''
+alias lastdump='firefox https://pastebin.com/6Cyb18tX'
